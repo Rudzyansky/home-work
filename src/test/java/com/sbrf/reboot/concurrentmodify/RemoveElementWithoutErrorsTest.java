105fecc33c269245deafde123a9058bbe5da9dce
Evgeny <evgeny@false.team>
2022-01-27 01:21:07 +0300

    Lesson 8 Task: Remove the list item by index. The list generation has been extracted into the setup method. The successRemoveElementWithoutErrors method removes a list item after the iterations. The successRemoveElementWithoutErrorsIteratorMethod method removes a list item during iteration using the Iterator::remove method. The successRemoveElementWithoutErrorsSynchronizeMethod method removes a list item after the iterations using the Thread and the synchronize.

--- src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
+++ src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
@@ -1,37 +1,90 @@
 package com.sbrf.reboot.concurrentmodify;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RemoveElementWithoutErrorsTest {
+    private List<Integer> list;
+
+    final int indexToDelete = 1;
+
+    final int beginElement = 1;
+    final int endElement = 3;
+
+    @BeforeEach
+    public void setup() {
+        list = IntStream.range(beginElement, endElement + 1).boxed().collect(Collectors.toList());
+    }
 
     @Test
     public void successConcurrentModificationException() {
-
-        List<Integer> list = new ArrayList() {{
-            add(1);
-            add(2);
-            add(3);
-        }};
-
         assertThrows(ConcurrentModificationException.class, () -> {
-            
             for (Integer integer : list) {
-                list.remove(1);
+                list.remove(indexToDelete);
             }
-            
         });
-
     }
 
     @Test
     public void successRemoveElementWithoutErrors() {
-
+        assertDoesNotThrow(() -> {
+            for (Integer integer : list) {
+            }
+            list.remove(indexToDelete);
+        });
     }
 
+    @Test
+    public void successRemoveElementWithoutErrorsIteratorMethod() {
+        assertDoesNotThrow(() -> {
+            int index = 0;
+            for (Iterator<Integer> it = list.iterator(); it.hasNext(); index++) {
+                it.next();
+                if (index == indexToDelete) {
+                    it.remove();
+                }
+            }
+        });
+    }
+
+    @Test
+    public void successRemoveElementWithoutErrorsSynchronizeMethod() {
+        assertDoesNotThrow(() -> {
+            final Object lock = new Object();
+
+            final Thread removeItemThread = new Thread(() -> {
+                synchronized (lock) {
+                    list.remove(indexToDelete);
+                }
+            });
+
+            synchronized (lock) {
+                for (Integer integer : list) {
+                    if (!removeItemThread.isAlive()) {
+                        /*
+                         * The thread payload will be executed after the top-level lock object is released.
+                         * Until that time, the thread will be running and waiting.
+                         * So isAlive will be true and only one thread will be running.
+                         */
+                        removeItemThread.start();
+                    }
+
+                    /*
+                     * Simulates some actions.
+                     * So that the thread has time to run before the end of iterations.
+                     */
+                    Thread.sleep(1);
+                }
+            }
+        });
+    }
 }
